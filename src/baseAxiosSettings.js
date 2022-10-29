3d4b2aba0be840e901c1cdde753e19a43c0e98cf
Oleg <semenchenko1852@mail.ru>
2022-10-29 21:05:42 +0300

    1.0.2 change token location

--- src/baseAxiosSettings.js
+++ src/baseAxiosSettings.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import {token} from "./components/settings";
+import {token} from "./settings";
 
 axios.defaults.baseURL = 'https://api.github.com';
 axios.defaults.headers.common['Authorization'] = "Bearer " + token
